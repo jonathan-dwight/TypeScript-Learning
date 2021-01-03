b4a60dda36e7cba0b3d0e35487ac213ef950c8eb
jonathan-dwight <jd.buendia0711@gmail.com>
2021-01-02 16:55:47 -0800

    compiler notes

---
+++ compiler-project/app.ts
@@ -0,0 +1,8 @@
+const userName = 'JD';
+
+console.log(userName);
+
+// THE LIB ALLOWS US TO KNOW WHAT TYPESCRIPT CAN USE
+// DEFAULT IS SET IF ITS NOT SELECTED - all es6 ways
+
+//SOURCE MAP in TS.Config allows use of debugging typescript on console
