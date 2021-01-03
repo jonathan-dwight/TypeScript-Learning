55abb89d7f659f2f5c44b25236687a0ab2955af7
jonathan-dwight <jd.buendia0711@gmail.com>
2021-01-02 17:47:08 -0800

    complete compiler notes

---
+++ compiler-project/src/analytics.ts
@@ -0,0 +1,11 @@
+// have to be true with parameters
+// variables can have an any
+
+// let logged; will not error out
+// logged = true
+
+function sendAnalytics(data: string) {
+    console.log(data);
+}
+
+sendAnalytics('the data');
--- compiler-project/src/app.ts
+++ compiler-project/src/app.ts
@@ -6,3 +6,5 @@ console.log(userName);
 // DEFAULT IS SET IF ITS NOT SELECTED - all es6 ways
 
 //SOURCE MAP in TS.Config allows use of debugging typescript on console
+
+// folder structure will keep consistent to dist folder
