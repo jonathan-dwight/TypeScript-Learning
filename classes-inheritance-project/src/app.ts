914832e96053adc4b0132a32726e4212424edd6b
jonathan-dwight <jd.buendia0711@gmail.com>
2021-01-08 14:27:44 -0800

    Add Inheritance

--- classes-inheritance-project/src/app.ts
+++ classes-inheritance-project/src/app.ts
@@ -1,105 +1,72 @@
-// Code goes here!
+// INTERFACE - describe how classes should be written
 
-//singleton only one instance of the class
+// difference between type and interface -- type can store other things
+// interface is clear that you want to define the structure
+// interface Person {
+//     name: string;
+//     age: number;
 
-// can only inherit
-abstract class Department {
+//     greet(phrase: string): void;
+// }
 
-    static fiscalYear = 2020;
-    // public name: string;
-    protected employees: string[] = []
-    // only accessible inside the class
-    // js doesnt enforce this because it only recently learned it
 
-    // below is a shortcut from the top
-    // can add readonly or writeonly
-    constructor(public name: string, protected id: string) {
-        // this.name = n;
-    }
+// let user1: Person;
 
-    //allows you to usue it anymwhere like Math.pow()
-    static createEmployee(name: string) {
-        return { name: name }
-    }
+// user1 = {
+//     name: 'JD',
+//     age: 30,
+//     greet(phrase: string) {
+//         console.log(phrase + ' ' + this.name)
+//     }
+// }
 
-    abstract describe(this: Department): void;
-    // other classes can reuse
 
-    addEmployee(employee: string) {
-        this.employees.push(employee);
-    }
+// can add readonly to interfaces or types
 
-    printEmployeeInformation() {
-        console.log(this.employees.length)
-        console.log(this.employees)
-    }
+// can also inherit interfaces 
+
+// type AddFn = (a: number, b: number) => number;
+
+interface AddFn {
+    (a: number, b:number): number;
 }
 
+let add: AddFn;
 
 
-class ITDepartment extends Department {
+interface Named {
+    readonly name?: string;
+    outputName?: string;
+    // question mark says it might exist but not 
+}
+
+// cna inherit from multiple interfaces with a comma
+interface Greetable extends Named {
+    greet(phrase: string): void;
+}
+
+class Person implements Greetable {
+    name?: string;
     
-    constructor(id: string, public admins: string[]) {
-        super(id, "IT"); // passes up properties
-    };
+    constructor(n?: string) {
+        if (n) {
+            this.name = n;
+        }
+    }
 
-    describe() {
-        console.log('IT Department - ID: ' + this.id);
+    greet(phrase: string) {
+        if (!this.name) {
+            console.log('Hi!')
+        } else {
+            console.log(phrase + ' ' + this.name)
+        }
     }
 }
 
+let user1: Greetable;
+
+user1 = new Person('Max');
+
+user1.greet('Hi there - I am')
 
 
-class AccountingDepartment extends Department {
-    private lastReport: string;
-    private static instance: AccountingDepartment;
-
-    //creating getter
-    get mostRecentReport() {
-        if (this.lastReport) {
-            return this.lastReport
-        } 
-        throw new Error('No report found')
-    }
-
-    set mostRecentReport(value: string) {
-        if (!value) {
-            throw new Error("Please pass in a new value")
-        }
-        this.addReport(value);
-    }
-
-    private constructor(id: string, private reports: string[]) {
-        super(id, 'Accounting');
-        this.lastReport = reports[0];
-    }
-
-    static getInstance() {
-        if (this.instance) {
-            return this.instance;
-        }
-        this.instance = new AccountingDepartment('d2', []);
-        return this.instance;
-    }
-
-    addReport(text: string) {
-        this.reports.push(text);
-        this.lastReport = text;
-    }
-
-    addEmployee(name: string) {
-        if (name === 'Max') {
-            return;
-        }
-        this.employees.push(name)
-        // changed it to protected to allow other extended classes to use it
-    }
-
-    describe() {
-        console.log("Accounting Department - ID " + this.id)
-    }
-
-}
-
-
-// static methods allow you to add things to class
